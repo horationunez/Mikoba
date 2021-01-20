54524d2ff8815a9b8847b383a4958002d4ef9978
Horacio Nunez <horacio@merthin.com>
2021-01-20 11:35:08 -0500

    saving

--- mikoba/UI/Components/KivaSvgImage.cs
+++ mikoba/UI/Components/KivaSvgImage.cs
@@ -1,6 +1,9 @@
 
 using System.Reflection;
+using System.Windows.Input;
 using SVG.Forms.Plugin.Abstractions;
+using Xamarin.Forms;
+using Xamarin.Forms.Markup;
 
 namespace mikoba.UI.Components
 {
@@ -8,9 +11,30 @@ namespace mikoba.UI.Components
     {
         private string _imageKey;
         
+        public static readonly BindableProperty CommandProperty =
+            BindableProperty.Create("Command", typeof(ICommand), typeof(ActionButton), null);
+
+        public ICommand Command
+        {
+            get
+            {
+                var command = (ICommand)GetValue(CommandProperty);
+                return command;
+            }
+            set { SetValue(CommandProperty, value); }
+        }
+
         public KivaSvgImage()
         {
             this.SvgAssembly = typeof(App).GetTypeInfo().Assembly;
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += (s, e) => {
+                if (Command != null)
+                {
+                    Command.Execute(null);
+                }
+            };
+            this.GestureRecognizers.Add(tapGestureRecognizer);
         }
 
         public string ImageKey
